48fb6060fdef71fb6446b1cad91e627f7c1021d2
anaeduarda023 <ana.eduarda.castro@escola.pr.gov.br>
2026-08-28 09:25:48 -0300

    .

--- script.js
+++ script.js
@@ -6,71 +6,73 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "pergunta deve ser escrita aqui",
+        enunciado: "O ciclo acabou e você precisa decidir qual será seu primeiro grande passo em direção a vida adulta. Qual camiho você escolhe?",
         alternativas: [
             {
-                texto: "alternativa letra A deve ser escrita aqui",
-                afirmacao: "Afirmação gerada se a pessoa escolher a letra A deve estar aqui "
+                texto: "Ingressar imediatamente na faculdade ou curso técnico para se especializar em sua área",
+                afirmacao: "Sua busca por especialização acadêmica te ajudou a construir uma base teórica sólida para sua carreira"
             },
             {
-                texto: "alternativa letra B deve ser escrita aqui",
-                afirmacao: "Afirmação gerada se a pessoa escolher a letra B deve estar aqui "
+                texto: "Entrar no mercado de trabalho para ganhar experiêriencia prática e independência financeira rápida.",
+                afirmacao: " A inserção rápida no mercado de trabalho trouxe maturidade e uma visão prática sobre o mundo corporativo."
             }
         ]
     },
     {
-        enunciado: "",
+        enunciado: "Você recebeu uma oportunidade de trabalho, mas ela exige que você saia da sua zona de conforto. O que você faz?",
         alternativas: [
             {
-                texto: "",
-                afirmacao: ""
+                texto: "Aceito a oportunidade e encaro o desafio.",
+                afirmacao: "Sua coragem para enfrentar mudanças pode ajudar no seu crescimento pessoal e profissional."
             },
             {
-                texto: "",
-                afirmacao: ""
+                texto: "Recuso e prefiro continuar em algo que já conheço.",
+                afirmacao: " Sua busca por segurança mostra que você valoriza estabilidade antes de assumir novos riscos."
             }
         ]
     },
     {
-        enunciado: "",
+        enunciado: "Você precisa escolher entre continuar estudando ou começar a trabalhar. Qual caminho você escolheria?",
         alternativas: [
             {
-                texto: "",
-                afirmacao: ""
+                texto: "Continuar estudando para aumentar minhas oportunidades.",
+                afirmacao: "Seu foco na educação  demonstra que você pensa no crescimento e nas oportunidades futuras. "
             },
             {
-                texto: "",
-                afirmacao: ""
+                texto: "Começar a trabalhar para conquistar minha independência.",
+                afirmacao: "Sua iniciativa mostra vontade de conquistar independência e experiência profissional."
             }
         ]
     },
     {
-        enunciado: "",
+        enunciado: "Você precisa tomar uma decisão importante sobre o futuro, mas sua família pensa diferente de você. O que você faz?",
         alternativas: [
             {
-                texto: "",
-                afirmacao: ""
+                texto: "Sigo minha própria decisão.",
+                afirmacao: "Sua independência mostra confiança para escolher o caminho que acredita ser melhor para você. "
             },
             {
-                texto: "",
-                afirmacao: ""
+                texto: "Sigo os conselhos da minha família.",
+                afirmacao: "Você valoriza a experiência e a opinião das pessoas que fazem parte da sua vida."
             }
         ]
     },
     {
-        enunciado: "",
+        enunciado: "Você conseguiu seu primeiro salário. Qual seria sua principal atitude?",
         alternativas: [
             {
-                texto: "",
-                afirmacao: ""
+                texto: "Guardar uma parte do dinheiro.",
+                afirmacao: "Sua preocupação com o futuro demonstra responsabilidade financeira."
             },
             {
-                texto: "",
-                afirmacao: ""
+                texto: "Usar o dinheiro para realizar algo que desejo.",
+                afirmacao: "Você valoriza suas conquistas e gosta de aproveitar os resultados do seu esforço."
             }
         ]
-    },
+    }
 ];
+
+
 
 
 let atual = 0;
